1ce137e91e6f955eb3bd9ea386f8e0d1353d168a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-07-10 23:51:36 -0400

    export get_emax, get_emin

--- src/ArbFloats.jl
+++ src/ArbFloats.jl
@@ -28,6 +28,7 @@ export ArbFloat,      # co-matched decimal rounding, n | round(hi,n,10) == round
        stringcompact, stringall, stringallcompact, stringpretty,
        smartvalue, smartstring, showsmart, showallcompact, showpretty,
        two, three, four, copymidpoint, copyradius, deepcopyradius,
+       get_emax, get_emin, 
        epsilon, trim, decompose, isexact, notexact,
        iszero, notzero, nonzero, isone, notone, notinteger,
        ispositive, notpositive, isnegative, notnegative,
