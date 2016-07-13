017c7a8c37951ecd7be126f84fca5b53c230c9cf
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-07-13 09:00:33 -0400

    Update runtests.jl

--- tests/runtests.jl
+++ tests/runtests.jl
@@ -10,22 +10,7 @@ include("test_prep.jl")
 
 #= begin tests =#
 
-@testset "numbers behave" begin
-  @testset "simple value ops" begin
-    @test af1 === af1
-    @test af1 == af1
-    @test af1 != af2
-    @test af1 <  af2
-    @test af1 <= af2 
-    @test af2 >  af1
-    @test af2 >= af1
-  end
-  @testset "value recovery" begin
-    @test  asin(log(acosh(cosh(exp(sin(afhalf)))))) > afhalf
-    @test  upperbound(asin(log(atanh(tanh(exp(sin(afhalf))))))) >= afhalf
-    @test  lowerbound(asin(log(atanh(tanh(exp(sin(afhalf))))))) <= afhalf
-  end
-end # numbers behave
+include("t_onetwo.jl")
 
 
 #= end tests =#
