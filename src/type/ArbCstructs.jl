0514ca02dbda190b2f81458c741ce6b2cce8cbe9
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2016-07-16 02:03:38 -0400

    make types be from AbstractFloat

--- src/type/ArbCstructs.jl
+++ src/type/ArbCstructs.jl
@@ -5,14 +5,14 @@
 =#
 
 
-type MagFloat
+type MagFloat <: AbstractFloat
     radius_exponentOf2::Int
     radius_significand::UInt   ## radius is unsigned (nonnegative), by definition
 end
 
     #       P is the precision in bits as a parameter
     #
-type ArfFloat{P}
+type ArfFloat{P} <: AbstractFloat
     exponentOf2 ::Int
     words_sgn::Int          ## Int, as words is an offset; lsb holds sign of significand
     significand1::UInt         ## UInt, as each significand word is a subspan of significand
@@ -22,12 +22,13 @@ end
 
     #       P is the precision in bits as a parameter
     #
-type ArbFloat{P}            ##     ArfFloat{P}
+type ArbFloat{P} <: AbstractFloat
+                               ##     ArfFloat{P}
     exponentOf2 ::Int          ##        exponentOf2
-    words_sgn::Int          ##        words_sgn
+    words_sgn::Int             ##        words_sgn
     significand1::UInt         ##        significand1
     significand2::UInt         ##        significand2
-                            ###    ArbMag{P}
+                               ###    ArbMag{P}
     radius_exponentOf2::Int    ####      radius_exponentOf2
     radius_significand::UInt   ####      radius_significand
 end
