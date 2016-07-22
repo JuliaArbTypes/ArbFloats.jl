d292e35906de9d491f8b970c707bdead501e3312
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-07-21 20:00:23 -0400

    add vector .op vector

--- src/math/arrayops.jl
+++ src/math/arrayops.jl
@@ -1,10 +1,14 @@
 for (op,dotop) in ((:(+),:(.+)), (:(-),:(.-)), (:(*),:(.*)), (:(/),:(./)))
   @eval begin
       function ($op){P}(x::ArbFloat{P}, y::Array{ArbFloat{P},1})
-          x ($dotop) y
+          return x ($dotop) y
       end
       function ($op){P}(x::Array{ArbFloat{P},1}, y::ArbFloat{P})
-          x ($dotop) y
+          return x ($dotop) y
       end
+      function ($op){P}(x::Array{ArbFloat{P},1}, y::Array{ArbFloat{P},1})
+          return x ($dotop) y
+      end
+  end
   end
 end
