66390377ae11e3280a029228e400fa827ba90bd3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-07-21 20:17:54 -0400

    correct syntax -- first pass at logic

--- src/math/arrayops.jl
+++ src/math/arrayops.jl
@@ -1,13 +1,13 @@
 for (op,dotop) in ((:(+),:(.+)), (:(-),:(.-)), (:(*),:(.*)), (:(/),:(./)))
   @eval begin
       function ($op){P}(x::ArbFloat{P}, y::Array{ArbFloat{P},1})
-          return x ($dotop) y
+          return ($dotop)(x, y)
       end
       function ($op){P}(x::Array{ArbFloat{P},1}, y::ArbFloat{P})
-          return x ($dotop) y
+          return ($dotop)(x, y)
       end
       function ($op){P}(x::Array{ArbFloat{P},1}, y::Array{ArbFloat{P},1})
-          return x ($dotop) y
+          return ($dotop)(x, y)
       end
   end
   end
