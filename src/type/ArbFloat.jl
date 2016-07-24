62e25dce2e8bb003e3144ea4629f2053e93eb6b0
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2016-07-24 09:50:40 -0400

    faster initializers

--- src/type/ArbFloat.jl
+++ src/type/ArbFloat.jl
@@ -48,7 +48,7 @@ function release{P}(x::ArbFloat{P})
 end
 
 function initializer{P}(::Type{ArbFloat{P}})
-    z = ArbFloat{P}(zero(Int), zero(UInt64), zero(Int64), zero(Int64), zero(Int), zero(UInt64))
+    z = ArbFloat{P}(0,0%UInt64,0,0,0,0%UInt64)
     ccall(@libarb(arb_init), Void, (Ptr{ArbFloat{P}}, ), &z)
     finalizer(z, release)
     return z
