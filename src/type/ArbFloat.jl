41f51f1f8613bc5f5ee3befaa134b670b3c37b9b
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2016-07-24 00:03:11 -0400

    reparam

--- src/type/ArbFloat.jl
+++ src/type/ArbFloat.jl
@@ -41,10 +41,12 @@ hash{P}(z::ArbFloat{P}, h::UInt) =
          (h $ hash(z.significand2$(~reinterpret(UInt,P)), hash_arbfloat_lo)
             $ hash_0_arbfloat_lo))
 
-@inline finalize{T<:ArbFloat}(::Type{T}) = ccall(@libarb(arb_clear), Void, (Ptr{T},), &x)
-@inline finalize{P}(::Type{ArbFloat{P}}) = ccall(@libarb(arb_clear), Void, (Ptr{ArbFloat{P}},), &x)
-@inline initial0{T<:ArbFloat}(::Type{T}) = ccall(@libarb(arb_init), Void, (Ptr{T},), &x)
-@inline initial0{P}(::Type{ArbFloat{P}}) = ccall(@libarb(arb_init), Void, (Ptr{ArbFloat{P}},), &x)
+@inline finalize{T<:ArbFloat}(x::Type{T}) = ccall(@libarb(arb_clear), Void, (Ptr{T},), &x)
+@inline finalize{P}(x::Type{ArbFloat{P}}) = ccall(@libarb(arb_clear), Void, (Ptr{ArbFloat{P}},), &x)
+
+@inline initial0{T<:ArbFloat}(x::Type{T}) = ccall(@libarb(arb_init), Void, (Ptr{T},), &x)
+@inline initial0{P}(x::Type{ArbFloat{P}}) = ccall(@libarb(arb_init), Void, (Ptr{ArbFloat{P}},), &x)
+
 # initialize and zero a variable of type ArbFloat
 function initializer{T<:ArbFloat}(::Type{T})
     P = precision(T)
