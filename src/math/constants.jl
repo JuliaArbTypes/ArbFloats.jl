ef3a3037ba624d8ab770074e094a80fbcf7997f7
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2016-07-25 21:42:18 -0400

    resolved

--- src/math/constants.jl
+++ src/math/constants.jl
@@ -1,5 +1,38 @@
 
 
+# special values
+
+INF{P}(::Type{ArbFloat{P}})     =  ArbFloat{P}("Inf")
+NAN{P}(::Type{ArbFloat{P}})     =  ArbFloat{P}("NaN")
+POSINF{P}(::Type{ArbFloat{P}})  =  ArbFloat{P}("Inf")
+NEGINF{P}(::Type{ArbFloat{P}})  =  ArbFloat{P}("-Inf")
+ZERO{P}(::Type{ArbFloat{P}})    =  ArbFloat{P}(0)
+ONE{P}(::Type{ArbFloat{P}})     =  ArbFloat{P}(1)
+#=
+TWO{P}(::Type{ArbFloat{P}})     =  ArbFloat{P}(2)
+QRTRPI{P}(::Type{ArbFloat{P}})  =  atan(ArbFloat{P}(1))
+PI{P}(::Type{ArbFloat{P}})      =  atan(ArbFloat{P}(1))*4
+INVPI{P}(::Type{ArbFloat{P}})   =  ArbFloat{P}( inv(atan(ArbFloat{32+P}(1))*4) )
+PHI{P}(::Type{ArbFloat{P}})     =  (sqrt(ArbFloat{P}(5)) + ArbFloat{P}(1)) / ArbFloat{P}(2)
+INVPHI{P}(::Type{ArbFloat{P}})  =  (sqrt(ArbFloat{P}(5)) - ArbFloat{P}(1)) / ArbFloat{P}(2)
+=#
+
+INF{T<:ArbFloat}(::Type{T})     =  INF(ArbFloat{precision(ArbFloat)})
+NAN{T<:ArbFloat}(::Type{T})     =  NAN(ArbFloat{precision(ArbFloat)})
+POSINF{T<:ArbFloat}(::Type{T})  =  POSINF(ArbFloat{precision(ArbFloat)})
+NEGINF{T<:ArbFloat}(::Type{T})  =  NEGINF(ArbFloat{precision(ArbFloat)})
+ZERO{T<:ArbFloat}(::Type{T})    =  ZERO(ArbFloat{precision(ArbFloat)})
+ONE{T<:ArbFloat}(::Type{T})     =  ONE(ArbFloat{precision(ArbFloat)})
+#=
+TWO{T<:ArbFloat}(::Type{T})     =  TWO(ArbFloat{precision(ArbFloat)})
+QRTRPI{T<:ArbFloat}(::Type{T})  =  QRTRPI(ArbFloat{precision(ArbFloat)})
+PI{T<:ArbFloat}(::Type{T})      =  PI(ArbFloat{precision(ArbFloat)})
+PHI{T<:ArbFloat}(::Type{T})     =  PHI(ArbFloat{precision(ArbFloat)})
+INVPHI{T<:ArbFloat}(::Type{T})  =  INVPHI(ArbFloat{precision(ArbFloat)})
+
+
+=#
+
 function PI{P}(::Type{ArbFloat{P}})
     z = initializer(ArbFloat{P})
     ccall(@libarb(arb_const_pi), Void, (Ptr{ArbFloat}, Int), &z, P)
