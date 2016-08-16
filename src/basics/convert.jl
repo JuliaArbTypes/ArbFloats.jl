e0c5fab481c551c87f0eb8b8077d1b30b3b51fe4
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2016-08-15 22:20:39 -0400

    specialize conversions

--- src/basics/convert.jl
+++ src/basics/convert.jl
@@ -12,15 +12,29 @@ macro ArbFloat(p,x)
     convert(ArbFloat{:($p)}, string(:($x)))
 end
 
+convert{T<:ArfFloat}(::Type{T}, x::T) = x
+convert{T<:ArbFloat}(::Type{T}, x::T) = x
 convert{P}(::Type{ArbFloat{P}}, x::ArbFloat{P}) = x
 convert{P}(::Type{ArfFloat{P}}, x::ArfFloat{P}) = x
 
+function convert{T<:ArfFloat,Q}(::Type{T}, x::ArfFloat{Q})
+   P = precision(T)
+   z = initializer(ArfFloat{P})
+   ccall(@libarb(arf_set_round), Void, (Ptr{ArfFloat{P}}, Ptr{ArfFloat{Q}}, Clong), &z, &x, Clong(P))
+   return z
+end
 function convert{P,Q}(::Type{ArfFloat{P}}, x::ArfFloat{Q})
    z = initializer(ArfFloat{P})
    ccall(@libarb(arf_set_round), Void, (Ptr{ArfFloat{P}}, Ptr{ArfFloat{Q}}, Clong), &z, &x, Clong(P))
    return z
 end
 
+function convert{T<:ArbFloat,Q}(::Type{T}, x::ArbFloat{Q})
+   P = precision(T)
+   z = initializer(ArbFloat{P})
+   ccall(@libarb(arb_set_round), Void, (Ptr{ArbFloat{P}}, Ptr{ArbFloat{Q}}, Clong), &z, &x, Clong(P))
+   return z
+end
 function convert{P,Q}(::Type{ArbFloat{P}}, x::ArbFloat{Q})
    z = initializer(ArbFloat{P})
    ccall(@libarb(arb_set_round), Void, (Ptr{ArbFloat{P}}, Ptr{ArbFloat{Q}}, Clong), &z, &x, Clong(P))
