e9dc73ceb319c74233a26232d10db420d315d135
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2016-08-15 21:14:44 -0400

    fixup

--- src/basics/convert.jl
+++ src/basics/convert.jl
@@ -299,10 +299,10 @@ function convert{I<:Integer,P}(::Type{I}, x::ArbFloat{P})
 end
 =#
 
-for T in (:Int128, :Int64, :Int32, :Int16, :Float64, :Float32, :Float16,
+for N in (:Int128, :Int64, :Int32, :Int16, :Float64, :Float32, :Float16,
           :(Rational{Int64}), :(Rational{Int32}), :(Rational{Int16}),
           :String)
-  @eval convert{T<:ArbFloat}(::Type{ArbFloat}, x::$T) = convert(ArbFloat{precision(ArbFloat)}, x)
+  @eval convert{T<:ArbFloat}(::Type{T}, x::$N) = convert(ArbFloat{precision(ArbFloat)}, x)
 end
 
 
