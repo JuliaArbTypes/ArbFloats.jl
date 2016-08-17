ff00a8792f218af83e0ce8b45552f91471a77965
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2016-08-17 04:19:47 -0400

    typo

--- src/math/elementary.jl
+++ src/math/elementary.jl
@@ -77,26 +77,26 @@ for (op,cfunc) in ((:^,:arb_pow), (:pow,:arb_pow))
       xx = ArbFloat{P}(x)
       z = initializer(ArbFloat{P})
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &xx, &y, P)
-      sx ? 1/z : z
+      return z
     end
     function ($op){P,I<:Integer}(x::ArbFloat{P}, y::I)
       sy,ay = signbit(y), abs(y)
       yy = ArbFloat{P}(ay)
       z = initializer(ArbFloat{P})
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, &yy, P)
-      sy ? 1/z : z
+      return sy ? 1/z : z
     end
     function ($op){P,R<:Real}(x::R, y::ArbFloat{P})
       xx = ArbFloat{P}(x)
       z = initializer(ArbFloat{P})
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &xx, &y, P)
-      z
+      return z
     end
     function ($op){P,R<:Real}(x::ArbFloat{P}, y::R)
       yy = ArbFloat{P}(y)
       z = initializer(ArbFloat{P})
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, &yy, P)
-      z
+      return z
     end
     function ($op){P}(x::ArbFloat{P}, y::ArbFloat{P})
       z = initializer(ArbFloat{P})
