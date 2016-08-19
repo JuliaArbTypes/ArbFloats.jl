6b50e117975d34dace82960a6e9944a44cb451e7
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2016-08-19 16:08:18 -0400

    using the direction Bill Hart suggested for resolving double free

--- src/type/ArbCstructs.jl
+++ src/type/ArbCstructs.jl
@@ -8,7 +8,19 @@
 type MagFloat <: AbstractFloat
     radius_exponentOf2::Int
     radius_significand::UInt   ## radius is unsigned (nonnegative), by definition
+
+    function MagFloat()
+         z = new()
+         ccall(@libarb(mag_init), Void, (Ptr{MagFloat}, ), &z)
+         finalizer(z, c_release_mag)
+         return z
+    end
 end
+
+function c_release_mag(x::MagFloat)
+  ccall(@libarb(mag_clear), Void, (Ptr{MagFloat}, ), &x)
+end
+
 
     #       P is the precision in bits as a parameter
     #
@@ -17,8 +29,18 @@ type ArfFloat{P} <: AbstractFloat
     nwords_sign::Int           ## Int, as words is an offset; lsb holds sign of significand
     significand1::UInt         ## UInt, as each significand word is a subspan of significand
     significand2::UInt         ##   the significand words are unsigned (sign is in nwords_sign)
+
+    function ArfFloat()
+         z = new{P}()
+         ccall(@libarb(arf_init), Void, (Ptr{ArfFloat{P}}, ), &z)
+         finalizer(z, c_release_arf)
+         return z
+    end
 end
 
+function c_release_arf{P}(x::ArfFloat{P})
+  ccall(@libarb(arf_clear), Void, (Ptr{ArfFloat{P}}, ), &x)
+end
 
     #       P is the precision in bits as a parameter
     #
@@ -31,4 +53,15 @@ type ArbFloat{P} <: AbstractFloat
                                ###    ArbMag{P}
     radius_exponentOf2::Int    ####      radius_exponentOf2
     radius_significand::UInt   ####      radius_significand
+
+    function ArbFloat()
+         z = new{P}()
+         ccall(@libarb(arb_init), Void, (Ptr{ArbFloat{P}}, ), &z)
+         finalizer(z, c_release_arb)
+         return z
+    end
+end
+
+function c_release_arb{P}(x::ArbFloat{P})
+  ccall(@libarb(arb_clear), Void, (Ptr{ArbFloat{P}}, ), &x)
 end
