c06d10c08318eaa5db99560f6adf420899a6b691
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2016-08-26 03:54:54 -0400

    add serialize/deserialize

--- src/support/compatable.jl
+++ src/support/compatable.jl
@@ -1,1 +1,9 @@
 
+function serialize{T<:ArbFloat}(ser::AbstractSerializer, a::T)
+    serialize_type(ser, T)
+    write(ser.io, stringall(a))
+end
+
+function deserialize{T<:ArbFloat}(ser::AbstractSerializer, ::Type{T})
+    T( read(ser.io, String) )
+end
