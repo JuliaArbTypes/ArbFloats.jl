5390f87115a92b627e65ea0918c7650a7f2a8bc6
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2016-08-30 03:24:58 -0400

    same

--- tests/TestAsType.jl
+++ tests/TestAsType.jl
@@ -25,6 +25,6 @@ afInf = ArbFloat(Inf)
 @test string(ArbFloat{24}(pi)) == "3.141593"
 @test string(ArbFloat{53}(catalan)) == "0.915965594177219"
 
-@test promote(ArbFloat(4), 3//5) == (4, ArbFloat(0.6))
+@test promote(ArbFloat(4), 8//2) == (ArbFloat(4), ArbFloat(4))
 
 end # module
