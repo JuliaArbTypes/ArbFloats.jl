c79ecf1a5bac024d715a3a306336cb24fb5b5b0d
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2016-09-06 06:31:41 -0400

    strings reworked (d) perhaps settled

--- src/basics/show.jl
+++ src/basics/show.jl
@@ -9,61 +9,61 @@ end
 show{T<:ArbFloat}(x::T) = show(STDOUT, x)
 
 function showsmall{T<:ArbFloat}(io::IO, x::T)
-    s = stringSmall(x)
+    s = stringsmall(x)
     print(io, s)
 end
 showsmall{T<:ArbFloat}(x::T) = showsmall(STDOUT, x)
 
 function showsmall_interval{T<:ArbFloat}(io::IO, x::T)
-    s = interval_stringSmall(x)
+    s = stringsmall_interval(x)
     print(io, s)
 end
 showsmall_interval{T<:ArbFloat}(x::T) = showsmall_interval(STDOUT, x)
 
 function showcompact{T<:ArbFloat}(io::IO, x::T)
-    s = stringCompact(x)
+    s = stringcompact(x)
     print(io, s)
 end
 showcompact{T<:ArbFloat}(x::T) = showcompact(STDOUT, x)
 
 function showcompact_interval{T<:ArbFloat}(io::IO, x::T)
-    s = interval_stringCompact(x)
+    s = stringcompact_interval(x)
     print(io, s)
 end
 showcompact_interval{T<:ArbFloat}(x::T) = showcompact_interval(STDOUT, x)
 
 function showmedium{T<:ArbFloat}(io::IO, x::T)
-    s = stringMedium(x)
+    s = stringmedium(x)
     print(io, s)
 end
 showmedium{T<:ArbFloat}(x::T) = showmedium(STDOUT, x)
 
 function show_interval{T<:ArbFloat}(io::IO, x::T)
-    s = interval_stringMedium(x)
+    s = stringmedium_interval(x)
     print(io, s)
 end
 show_interval{T<:ArbFloat}(x::T) = show_interval(STDOUT, x)
 
 function showlarge{T<:ArbFloat}(io::IO, x::T)
-    s = stringLarge(x)
+    s = stringlarge(x)
     print(io, s)
 end
 showlarge{T<:ArbFloat}(x::T) = showlarge(STDOUT, x)
 
 function showlarge_interval{T<:ArbFloat}(io::IO, x::T)
-    s = interval_stringLarge(x)
+    s = stringlarge_interval(x)
     print(io, s)
 end
 showlarge_interval{T<:ArbFloat}(x::T) = showlarge_interval(STDOUT, x)
 
 function showall{T<:ArbFloat}(io::IO, x::T)
-    s = stringAll(x)
+    s = stringall(x)
     print(io, s)
 end
 showall{T<:ArbFloat}(x::T) = showall(STDOUT, x)
 
 function showall_interval{T<:ArbFloat}(io::IO, x::T)
-    s = interval_stringAll(x)
+    s = stringall_interval(x)
     print(io, s)
 end
 showall_interval{T<:ArbFloat}(x::T) = showall_interval(STDOUT, x)
@@ -135,26 +135,43 @@ showmany{T<:ArbFloat}(x::Vector{T}, stringformer::Function) =
 
 
 show{P,N}(io::IO, x::NTuple{N,ArbFloat{P}}) = showmany(io, x, string)
-showsmall{P,N}(io::IO, x::NTuple{N,ArbFloat{P}}) = showmany(io, x, stringSmall)
-showcompact{P,N}(io::IO, x::NTuple{N,ArbFloat{P}}) = showmany(io, x, stringCompact)
-showlarge{P,N}(io::IO, x::NTuple{N,ArbFloat{P}}) = showmany(io, x, stringLarge)
-showmedium{P,N}(io::IO, x::NTuple{N,ArbFloat{P}}) = showmany(io, x, stringMedium)
-showall{P,N}(io::IO, x::NTuple{N,ArbFloat{P}}) = showmany(io, x, stringAll)
+showsmall{P,N}(io::IO, x::NTuple{N,ArbFloat{P}}) = showmany(io, x, stringsmall)
+showcompact{P,N}(io::IO, x::NTuple{N,ArbFloat{P}}) = showmany(io, x, stringcompact)
+showlarge{P,N}(io::IO, x::NTuple{N,ArbFloat{P}}) = showmany(io, x, stringlarge)
+showmedium{P,N}(io::IO, x::NTuple{N,ArbFloat{P}}) = showmany(io, x, stringmedium)
+showall{P,N}(io::IO, x::NTuple{N,ArbFloat{P}}) = showmany(io, x, stringall)
+
+show_interval{P,N}(io::IO, x::NTuple{N,ArbFloat{P}}) = showmany(io, x, stringmedium_interval)
+showsmall_interval{P,N}(io::IO, x::NTuple{N,ArbFloat{P}}) = showmany(io, x, stringsmall_interval)
+showcompact_interval{P,N}(io::IO, x::NTuple{N,ArbFloat{P}}) = showmany(io, x, stringcompact_interval)
+showlarge_interval{P,N}(io::IO, x::NTuple{N,ArbFloat{P}}) = showmany(io, x, stringlarge_interval)
+showmedium_interval{P,N}(io::IO, x::NTuple{N,ArbFloat{P}}) = showmany(io, x, stringmedium_interval)
+showall_interval{P,N}(io::IO, x::NTuple{N,ArbFloat{P}}) = showmany(io, x, stringall_interval)
 
 # showsmart is not a Base show function, it needs explict version without io parameter
 showsmart{P,N}(io::IO, x::NTuple{N,ArbFloat{P}}) = showmany(io, x, smartstring)
 showsmart{P,N}(x::NTuple{N,ArbFloat{P}}) = showmany(STDOUT, x, smartstring)
 
 show{P}(io::IO, x::Vector{ArbFloat{P}}) = showmany(io, x, string)
-showall{P}(io::IO, x::Vector{ArbFloat{P}}) = showmany(io, x, stringAll)
-showcompact{P}(io::IO, x::Vector{ArbFloat{P}}) = showmany(io, x, stringCompact)
+showall{P}(io::IO, x::Vector{ArbFloat{P}}) = showmany(io, x, stringall)
+showsmall{P}(io::IO, x::Vector{ArbFloat{P}}) = showmany(io, x, stringsmall)
+showcompact{P}(io::IO, x::Vector{ArbFloat{P}}) = showmany(io, x, stringcompact)
+show_interval{P}(io::IO, x::Vector{ArbFloat{P}}) = showmany(io, x, stringmedium_interval)
+showsmall_interval{P}(io::IO, x::Vector{ArbFloat{P}}) = showmany(io, x, stringsmall_interval)
+showcompact_interval{P}(io::IO, x::Vector{ArbFloat{P}}) = showmany(io, x, stringcompact_interval)
+showall_interval{P}(io::IO, x::Vector{ArbFloat{P}}) = showmany(io, x, stringall_interval)
 # showsmart is not a Base show function, it needs explict version without io parameter
 showsmart{P}(io::IO, x::Vector{ArbFloat{P}}) = showmany(io, x, smartstring)
 showsmart{P}(x::Vector{ArbFloat{P}}) = showmany(STDOUT, x, smartstring)
 
 show{P,N}(io::IO, x::Vararg{ArbFloat{P},N}) = showmany(io, x, string)
-showall{P,N}(io::IO, x::Vararg{ArbFloat{P},N}) = showmany(io, x, stringAll)
-showcompact{P,N}(io::IO, x::Vararg{ArbFloat{P},N}) = showmany(io, x, stringCompact)
+showsmall{P,N}(io::IO, x::Vararg{ArbFloat{P},N}) = showmany(io, x, stringsmall)
+showcompact{P,N}(io::IO, x::Vararg{ArbFloat{P},N}) = showmany(io, x, stringcompact)
+showall{P,N}(io::IO, x::Vararg{ArbFloat{P},N}) = showmany(io, x, stringall)
+show_interval{P,N}(io::IO, x::Vararg{ArbFloat{P},N}) = showmany(io, x, stringmedium_interval)
+showsmall_interval{P,N}(io::IO, x::Vararg{ArbFloat{P},N}) = showmany(io, x, stringsmall_interval)
+showcompact_interval{P,N}(io::IO, x::Vararg{ArbFloat{P},N}) = showmany(io, x, stringcompact_interval)
+showall_interval{P,N}(io::IO, x::Vararg{ArbFloat{P},N}) = showmany(io, x, stringall_interval)
 # showsmart is not a Base show function, it needs explict version without io parameter
 showsmart{P,N}(io::IO, x::Vararg{ArbFloat{P},N}) = showmany(io, x, smartstring)
 showsmart{P,N}(x::Vararg{ArbFloat{P},N}) = showmany(STDOUT, x, smartstring)
