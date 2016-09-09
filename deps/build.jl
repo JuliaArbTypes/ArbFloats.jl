2990d436f2dbbe7debfcb4fc4afeadbe16458f98
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2016-09-09 00:47:21 -0400

    do not REQUIRE Nemo, do check for it in build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,12 +1,29 @@
 using BinDeps
 using Compat
 
-@BinDeps.setup
 
-#=
-libflint = library_dependency("libflint", aliases=["libflint", "flint"])
-libarb = library_dependency("libarb", aliases=["libarb", "arb"])
+# ensure the requisite libraries are available
 
-@BinDeps.install Dict([(:libflint, :libflint), (:libarb, :libarb)])
-=#
-@BinDeps.install
+const noNemo = "Nemo.jl is not found:\n  Pkg.rm(\"Nemo\"); Pkg.add(\"Nemo\"); quit()\n  Pkg.rm(\"ArbFloats\");Pkg.add(\"ArbFloats\");"
+const reNemo = "Nemo.jl is not as expected:\n  Pkg.rm(\"Nemo\"); Pkg.add(\"Nemo\"); quit()\n  Pkg.rm(\"ArbFloats\");Pkg.add(\"ArbFloats\");"
+
+
+function package_directory(pkgName::String)
+    pkgdir = Base.find_in_path(pkgName)
+    nothing == pkgdir && throw(ErrorException(noNemo))
+    return abspath(joinpath(split(pkgdir, pkgName)[1], pkgName))
+end    
+
+function library_filepath(libsdir::String, filenames::Vector{String}, libname::String)
+    libfile = filenames[ findfirst([startswith(x,libname) for x in filenames]) ]
+    return joinpath( libsdir, libfile )
+end
+
+NemoLibsDir = abspath(joinpath( package_directory("Nemo"), "local/lib"))
+libFiles = readdir(NemoLibsDir)
+
+libarb   = library_filepath( NemoLibsDir, libFiles, "libarb"  )
+libflint = library_filepath( NemoLibsDir, libFiles, "libflint")
+
+isfile(libarb)   || throw(ErrorException(reNemo))
+isfile(libflint) || throw(ErrorException(reNemo))
