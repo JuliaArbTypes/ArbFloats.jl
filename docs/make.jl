dda5da31be6253e3ae9cc0972215e63b78a28559
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2016-09-08 20:16:55 -0400

    begin docs construction

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,19 @@
 using Documenter, ArbFloats
 
-makedocs(modules=[ArbFloats], doctest=true)
+makedocs(
+    sitename = "ArbFloats.jl"
+    modules  = [ArbFloats], 
+    format   = Documenter.Formats.HTML,
+    clean    = false,
+    pages    = Any["Home" => "index.md"],
+    doctest  = true,
+)
 
 deploydocs(
-    deps = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
-    repo = "github.com/JuliaArbTypes/ArbFloats.jl.git",
+    target = "build",
+    deps   = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
+    make   = nothing,
+    repo   = "github.com/JuliaArbTypes/ArbFloats.jl.git",
     julia  = "0.5.0-rc3",
-    osname = "linux")
+    osname = "linux",
+)
