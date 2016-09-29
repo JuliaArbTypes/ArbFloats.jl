ba90545441c11601691d4ec09c2a8597030858d0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-09-29 09:19:14 -0400

    Update sort.jl

--- src/basics/sort.jl
+++ src/basics/sort.jl
@@ -1,2 +1,2 @@
 sort{T<:ArbFloat}(xs::Vector{T}, lt::Function=<, rev::Bool=false) = sort(xs, alg=QuickSort, lt=lt, rev=rev)
-strictsort{T<:ArbFloat}(xs::Vector{T}, lt::Function=prec, rev::Bool=false) = sort(xs, alg=MergeSort, lt=lt, rev=rev)
+strictsort{T<:ArbFloat}(xs::Vector{T}, lt::Function=succ, rev::Bool=false) = sort(xs, alg=MergeSort, lt=lt, rev=rev)
