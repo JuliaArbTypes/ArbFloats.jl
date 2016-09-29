e71c2d0adb2eb2d47d2ef2d8a61fa6f64fcd8fce
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-09-29 09:24:13 -0400

    Update sort.jl

--- src/basics/sort.jl
+++ src/basics/sort.jl
@@ -1,2 +1,5 @@
 sort{T<:ArbFloat}(xs::Vector{T}, lt::Function=<, rev::Bool=false) = sort(xs, alg=QuickSort, lt=lt, rev=rev)
-strictsort{T<:ArbFloat}(xs::Vector{T}, lt::Function=succ, rev::Bool=false) = sort(xs, alg=MergeSort, lt=lt, rev=rev)
+function sortIntervals{T<:ArbFloat}(xs::Vector{T}, rev::Bool=false)
+   lessthan = rev ? pred : succ  
+   return sort(xs, alg=MergeSort, lt=lessthan)
+end
