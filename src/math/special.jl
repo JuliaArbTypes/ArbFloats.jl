10f8ca55a1b81a6813eaedebddd9c8d4366847e7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-10-17 23:54:10 -0400

    use initializer(T), faster

--- src/math/special.jl
+++ src/math/special.jl
@@ -3,7 +3,7 @@ for (op,cfunc) in ((:factorial,:arb_fac_ui), (:doublefactorial,:arb_doublefac_ui
     function ($op){P}(x::ArbFloat{P})
       signbit(x) && throw(ErrorException("Domain Error: argument is negative"))
       y = trunc(UInt, x)
-      z = ArbFloat{P}()
+      z = initializer(ArbFloat{P})
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, UInt, Int), &z, y, P)
       z
     end
@@ -20,19 +20,19 @@ for (op,cfunc) in ((:risingfactorial,:arb_rising),)
   @eval begin
     function ($op){P}(x::ArbFloat{P}, y::ArbFloat{P})
       signbit(x) && throw(ErrorException("Domain Error: argument is negative"))
-      z = ArbFloat{P}()
+      z = initializer(ArbFloat{P})
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, &y, P)
       z
     end
     function ($op){R<:Real,P}(xx::R, y::ArbFloat{P}, prec::Int=P)
       x = convert(ArbFloat{P},xx)
-      z = ArbFloat{P}()
+      z = initializer(ArbFloat{P})
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, &y, P)
       z
     end
     function ($op){R<:Real,P}(x::ArbFloat{P}, yy::R, prec::Int=P)
       y = convert(ArbFloat{P},yy)
-      z = ArbFloat{P}()
+      z = initializer(ArbFloat{P})
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, &y, P)
       z
     end
@@ -40,7 +40,7 @@ for (op,cfunc) in ((:risingfactorial,:arb_rising),)
       P = precision(ArbFloat)
       x = convert(ArbFloat{P},xx)
       y = convert(ArbFloat{P},yy)
-      z = ArbFloat{P}()
+      z = initializer(ArbFloat{P})
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, &y, P)
       z
     end
@@ -50,19 +50,19 @@ end
 for (op,cfunc) in ((:agm, :arb_agm), (:polylog, :arb_polylog))
   @eval begin
     function ($op){P}(x::ArbFloat{P}, y::ArbFloat{P}, prec::Int=P)
-      z = ArbFloat{P}()
+      z = initializer(ArbFloat{P})
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, &y, P)
       z
     end
     function ($op){R<:Real,P}(xx::R, y::ArbFloat{P}, prec::Int=P)
       x = convert(ArbFloat{P},xx)
-      z = ArbFloat{P}()
+      z = initializer(ArbFloat{P})
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, &y, P)
       z
     end
     function ($op){R<:Real,P}(x::ArbFloat{P}, yy::R, prec::Int=P)
       y = convert(ArbFloat{P},yy)
-      z = ArbFloat{P}()
+      z = initializer(ArbFloat{P})
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, &y, P)
       z
     end
@@ -70,7 +70,7 @@ for (op,cfunc) in ((:agm, :arb_agm), (:polylog, :arb_polylog))
       P = precision(ArbFloat)
       x = convert(ArbFloat{P},xx)
       y = convert(ArbFloat{P},yy)
-      z = ArbFloat{P}()
+      z = initializer(ArbFloat{P})
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, &y, P)
       z
     end
