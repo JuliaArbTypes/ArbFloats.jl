0e74d54daf3d0b62a1f8c6a499480c0afedfbd53
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-10-24 21:51:09 -0400

    Update TestAsNumber.jl

--- test/TestAsNumber.jl
+++ test/TestAsNumber.jl
@@ -15,6 +15,9 @@ afInf = ArbFloat(Inf)
 @test af2 >  af1
 @test af2 >= af1
 
+@test min(af1,af2) != af2
+@test max(af1,af2) == af2
+
 @test  asin(log(acosh(cosh(exp(sin(afhalf)))))) > afhalf
 @test  upperbound(asin(log(atanh(tanh(exp(sin(afhalf))))))) >= afhalf
 @test  lowerbound(asin(log(atanh(tanh(exp(sin(afhalf))))))) <= afhalf
