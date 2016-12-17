9a46a0d2bb291399c4194f884f294df86aca6f8e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-12-17 18:43:16 -0500

    Update support.jl

--- test/benchmark/support.jl
+++ test/benchmark/support.jl
@@ -2,63 +2,25 @@ using BenchmarkTools
 using ArbFloats
 
 macro setprecisions(nbits)
-    quote begin
-        setprecision(BigFloat, $nbits)
-        setprecision(ArbFloat, $nbits)
-    end end
+  quote begin
+    setprecision(BigFloat, $nbits)
+    setprecision(ArbFloat, $nbits)
+  end end
 end
 
 macro vals(v)
-    convert(BigFloat,v), convert(ArbFloat,v)
+   quote 
+     convert(BigFloat,$v), convert(ArbFloat,$v)
+   end     
 end    
 
 
 macro bench(f,val)
-    quote begin
-        local bfrunner, bfns
-        bfrunner = @benchmarkable ($f)($val)
-        tune!(bfrunner)
-        bfns = run(bfrunner)
-        return bfns.times[1]
-     end end
-end
-
-
-# bigfloat_val, arbfloat_val = convetvalue( 6.125 )
-macro getvalues(val)
-    quote begin
-        local strval, bigfloat, bigrecip, arbfloat, arbrecip
-        strval = string($val)
-        bigfloat = parse(BigFloat,strval)
-        bigrecip = one(BigFloat)/bigfloat
-        arbfloat = ArbFloat(strval)
-        arbrecip = one(ArbFloat)/arbfloat
-        return (bigfloat, arbfloat, bigrecip, arbrecip)
-    end end 
-end
-
-        
-
-# bigfloat_val, reciprocal_bigfloat_val, arbfloat_val, reciprocal_arbfloat_val = getvalues( 6.125 )
-macro getvalues(val)
   quote begin
-        local strval, bigfloat, bigrecip, arbfloat, arbrecip
-        strval = string($val)
-        bigfloat = parse(BigFloat,strval)
-        bigrecip = one(BigFloat)/bigfloat
-        arbfloat = ArbFloat(strval)
-        arbrecip = one(ArbFloat)/arbfloat
-        return (bigfloat, arbfloat, bigrecip, arbrecip)
-  end end 
+    local bfrunner, bfns
+    bfrunner = @benchmarkable ($f)($val)
+    tune!(bfrunner)
+    bfns = run(bfrunner)
+    return bfns.times[1]
+  end end
 end
-
-        
-macro bfrun(f,val)
-  quote begin
-        local bfrunner, bfns
-        bfrunner = @benchmarkable ($f)($val)
-        tune!(bfrunner)
-        bfns = run(bfrunner)
-        return bfns.times[1]
-        end end
-    end  end
