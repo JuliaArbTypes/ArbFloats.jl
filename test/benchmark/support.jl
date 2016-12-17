7580b9bd026c05244cbf9cf61abbd63889170b72
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-12-17 18:38:15 -0500

    Update support.jl

--- test/benchmark/support.jl
+++ test/benchmark/support.jl
@@ -8,6 +8,37 @@ macro setprecisions(nbits)
     end end
 end
 
+macro vals(v)
+    convert(BigFloat,v), convert(ArbFloat,v)
+end    
+
+
+macro bench(f,val)
+    quote begin
+        local bfrunner, bfns
+        bfrunner = @benchmarkable ($f)($val)
+        tune!(bfrunner)
+        bfns = run(bfrunner)
+        return bfns.times[1]
+     end end
+end
+
+
+# bigfloat_val, arbfloat_val = convetvalue( 6.125 )
+macro getvalues(val)
+    quote begin
+        local strval, bigfloat, bigrecip, arbfloat, arbrecip
+        strval = string($val)
+        bigfloat = parse(BigFloat,strval)
+        bigrecip = one(BigFloat)/bigfloat
+        arbfloat = ArbFloat(strval)
+        arbrecip = one(ArbFloat)/arbfloat
+        return (bigfloat, arbfloat, bigrecip, arbrecip)
+    end end 
+end
+
+        
+
 # bigfloat_val, reciprocal_bigfloat_val, arbfloat_val, reciprocal_arbfloat_val = getvalues( 6.125 )
 macro getvalues(val)
     quote begin
@@ -16,7 +47,7 @@ macro getvalues(val)
         bigfloat = parse(BigFloat,strval)
         bigrecip = one(BigFloat)/bigfloat
         arbfloat = ArbFloat(strval)
-        arbrecip = one(ArbFloat)//arbfloat
+        arbrecip = one(ArbFloat)/arbfloat
         return (bigfloat, arbfloat, bigrecip, arbrecip)
     end end 
 end
