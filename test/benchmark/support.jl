e0fdf990dd9238c39aa1e68e729e0c287d28b73e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-12-18 00:02:25 -0500

    Update support.jl

--- test/benchmark/support.jl
+++ test/benchmark/support.jl
@@ -101,31 +101,31 @@ const fp_precise_bits = [128*i for i in 1:16];
 
 println( "    bits: ",fp_precise_bits' );println()
 
-v0 = Float64(golden); v1 = sqrt(v0); v2 = 1/sqrt(5);
-# mul_slowerby = nbit_bigfloat_slowerby( (*), v1, v2, fp_precise_bits); println("    mul :",mul_slowerby' )
-# div_slowerby = nbit_bigfloat_slowerby( (/), v1, v2, fp_precise_bits); println("    div :", div_slowerby' )
+g = Float64(golden); recipg = 1/g; ten=10.0; tenth = (1/ten);
+mul_slowerby = nbit_bigfloat_slowerby( (*), g, recipg, fp_precise_bits); println("    mul :",mul_slowerby' )
+div_slowerby = nbit_bigfloat_slowerby( (/), g, recipg, fp_precise_bits); println("    div :", div_slowerby' )
 # sqrt_slowerby = nbit_bigfloat_slowerby( (sqrt), v1, v2, fp_precise_bits); println("    sqrt :", sqrt_slowerby' )
-exp_slowerby = nbit_bigfloat_slowerby( (exp), v0, fp_precise_bits);   println( "    exp : ", exp_slowerby' ) 
-log_slowerby = nbit_bigfloat_slowerby( (log), v0, fp_precise_bits);   println( "    log : ", log_slowerby' )
-cos_slowerby = nbit_bigfloat_slowerby( (cos), v0, fp_precise_bits);   println( "    cos : ", cos_slowerby' )
-tan_slowerby = nbit_bigfloat_slowerby( (tan), v0, fp_precise_bits);   println( "    tan : ", tan_slowerby' )
-acos_slowerby = nbit_bigfloat_slowerby( (acos), v0, fp_precise_bits); println( "    acos: ", acos_slowerby' )
-atan_slowerby = nbit_bigfloat_slowerby( (atan), v0, fp_precise_bits); println( "    atan: ", atan_slowerby' )
-cosh_slowerby = nbit_bigfloat_slowerby( (cosh), v0, fp_precise_bits);  println( "    cosh: ", cosh_slowerby' )
-tanh_slowerby = nbit_bigfloat_slowerby( (tanh), v0, fp_precise_bits);  println( "    tanh: ", tanh_slowerby' )
-acosh_slowerby = nbit_bigfloat_slowerby( (acosh), v0, fp_precise_bits);println( "    acosh: ", acosh_slowerby' )
-atanh_slowerby = nbit_bigfloat_slowerby( (atanh), v0, fp_precise_bits);println( "    atanh: ", atanh_slowerby' )
+exp_slowerby = nbit_bigfloat_slowerby( (exp), g, fp_precise_bits);   println( "    exp : ", exp_slowerby' ) 
+log_slowerby = nbit_bigfloat_slowerby( (log), g, fp_precise_bits);   println( "    log : ", log_slowerby' )
+cos_slowerby = nbit_bigfloat_slowerby( (cos), g, fp_precise_bits);   println( "    cos : ", cos_slowerby' )
+tan_slowerby = nbit_bigfloat_slowerby( (tan), g, fp_precise_bits);   println( "    tan : ", tan_slowerby' )
+acos_slowerby = nbit_bigfloat_slowerby( (acos), recipg, fp_precise_bits); println( "    acos: ", acos_slowerby' )
+atan_slowerby = nbit_bigfloat_slowerby( (atan), recipg, fp_precise_bits); println( "    atan: ", atan_slowerby' )
+cosh_slowerby = nbit_bigfloat_slowerby( (cosh), g, fp_precise_bits);  println( "    cosh: ", cosh_slowerby' )
+tanh_slowerby = nbit_bigfloat_slowerby( (tanh), g, fp_precise_bits);  println( "    tanh: ", tanh_slowerby' )
+acosh_slowerby = nbit_bigfloat_slowerby( (acosh), g, fp_precise_bits);println( "    acosh: ", acosh_slowerby' )
+atanh_slowerby = nbit_bigfloat_slowerby( (atanh), recipg, fp_precise_bits);println( "    atanh: ", atanh_slowerby' )
 
-gamma_slowerby = nbit_bigfloat_slowerby( (gamma), v0, fp_precise_bits);println( "    gamma:  ", gamma_slowerby' )
-digamma_slowerby = nbit_bigfloat_slowerby( (digamma), v0, fp_precise_bits);println( "    digamma:  ", digamma_slowerby' )
-zeta_slowerby = nbit_bigfloat_slowerby( (zeta), v0, fp_precise_bits);println( "     zeta:  ", zeta_slowerby' )
+gamma_slowerby = nbit_bigfloat_slowerby( (gamma), g, fp_precise_bits);println( "    gamma:  ", gamma_slowerby' )
+digamma_slowerby = nbit_bigfloat_slowerby( (digamma), g, fp_precise_bits);println( "    digamma:  ", digamma_slowerby' )
+zeta_slowerby = nbit_bigfloat_slowerby( (zeta), g, fp_precise_bits);println( "     zeta:  ", zeta_slowerby' )
 
 println();println( "    bits: ",fp_precise_bits' )
 
 
 println("     *      : ", mul_slowerby' );
 println("     /      : ", div_slowerby' );
-println("    cbrt    : ", cbrt_slowerby' );
+#println("    cbrt    : ", cbrt_slowerby' );
 println( "    exp    : ", exp_slowerby' ); 
 println( "    log    : ", log_slowerby' );
 println( "    cos    : ", cos_slowerby' );
