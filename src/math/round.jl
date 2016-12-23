316a4d2ae446360b679dfd3fafeeb039070b606a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-12-23 07:36:39 -0500

    Update round.jl

--- src/math/round.jl
+++ src/math/round.jl
@@ -32,7 +32,7 @@ function sigBitsToUse(prec::Int, sig::Int, base::Int)
 end
 
 function round{P}(x::ArbFloat{P}, places::Int=P, base::Int=2)
-    base==2 | base==10 || throw(ErrorException(string("Expecting base in (2,10), radix ",base," is not supported.")))
+    (base==2 | base==10) || throw(ErrorException(string("Expecting base in (2,10), radix ",base," is not supported.")))
     # places = max(1, abs(places))
     sigbits = base==2 ? places : digits_to_rounded_bits(places)
     z = initializer(ArbFloat{P})
