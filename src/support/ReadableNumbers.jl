12882923c49dc2baf4f6c1badf2ce8c57157c595
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-12-23 09:32:48 -0500

    Create ReadableNumbers.jl

---
+++ src/support/ReadableNumbers.jl
@@ -0,0 +1,26 @@
+
+module ReadableNumbers
+
+#=
+    generating and showing prettier numeric strings
+=#     
+
+export ReadableNumStyle, readable, show_readable 
+
+import Base: STDOUT, parse
+
+
+if VERSION < v"0.6"
+    split(str::String, sep::Char=" ") = map(String, Base.split(str, sep))  # do not work with SubStrings
+end    
+
+# determine locale convention for the fractional (decimal) point
+const LOCALE_STR = string( 1.5 )
+const FRACPOINT  = LOCALE_STR[ nextind(LOCALE_STR, 1) ]
+
+include("type.jl")
+include("dothework.jl")
+include("iohelp.jl")
+
+
+end # module
