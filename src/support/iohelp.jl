a7f0378aac1911e4d4e032224e6ccbd722b5749c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-12-23 09:34:07 -0500

    Create iohelp.jl

---
+++ src/support/iohelp.jl
@@ -0,0 +1,23 @@
+import Base: STDOUT
+
+
+# show 
+
+function show_readable{T<:Real}(io::IO, x::T)
+    str = readable(x)
+    print(io, str)
+end
+
+function show_readable{T<:Real}(x::T)
+    str = readable(x, etc)
+    print(STDOUT, str)
+end    
+
+
+# parse readable numeric strings
+
+parse{T<:Union{Signed,AbstractFloat}}(::Type{T}, s::String, ch::Char) =
+    parse(T, join(split(s,ch),""))
+
+parse{T<:AbstractFloat}(::Type{T}, s::String, ch1::Char, ch2::Char) =
+    parse(T, join(split(s,(ch1,ch2)),""))
