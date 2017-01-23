444edbfc3d904071a33eadc1acc06440b26452ed
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-01-23 15:14:36 -0500

    remove extra versions

--- src/math/arith.jl
+++ src/math/arith.jl
@@ -8,9 +8,6 @@
 function signbit{P}(x::ArbFloat{P})
     0 != ccall(@libarb(arb_is_negative), Int, (Ptr{ArbFloat{P}},), &x)
 end
-function signbit{T<:ArbFloat}(x::T)
-    0 != ccall(@libarb(arb_is_negative), Int, (Ptr{ArbFloat},), &x)
-end
 
 function abs{P}(x::ArbFloat{P})
     z  = initializer(ArbFloat{P})
@@ -21,24 +18,9 @@ function abs{P}(x::ArbFloat{P})
     ccall(@libarb(arb_set_interval_arf), Void, (Ptr{ArbFloat{P}}, Ptr{ArfFloat{P}}, Ptr{ArfFloat{P}}, Int), &z, &lo, &hi, P)
     return z
 end
-function abs{T<:ArbFloat}(x::T)
-    P = precision(T)
-    z  = initializer(ArbFloat{P})
-    lo = initializer(ArfFloat{P})
-    hi = initializer(ArfFloat{P})
-    ccall(@libarb(arb_get_abs_lbound_arf), Void, (Ptr{ArfFloat}, Ptr{ArbFloat}, Int), &lo, &x, P) 
-    ccall(@libarb(arb_get_abs_ubound_arf), Void, (Ptr{ArfFloat}, Ptr{ArbFloat}, Int), &hi, &x, P)
-    ccall(@libarb(arb_set_interval_arf), Void, (Ptr{ArbFloat}, Ptr{ArfFloat}, Ptr{ArfFloat}, Int), &z, &lo, &hi, P)
-    return z
-end
 
 function abs2{T<:ArbFloat}(x::T)
     a = abs(x)
-    return a*a
-end
-
-function absz2{T<:ArbFloat}(x::T)
-    a = absz(x)
     return a*a
 end
 
@@ -47,12 +29,6 @@ for (op,cfunc) in ((:-,:arb_neg), (:sign, :arb_sgn), (:absz, :arb_abs))
     function ($op){P}(x::ArbFloat{P})
       z = initializer(ArbFloat{P})
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat{P}}, Ptr{ArbFloat{P}}), &z, &x)
-      return z
-    end
-    function ($op){T<:ArbFloat}(x::T)
-      P = precision(T)
-      z = initializer(ArbFloat{P})
-      ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}), &z, &x)
       return z
     end
   end
@@ -75,12 +51,6 @@ for (op,cfunc) in ((:+,:arb_add), (:-, :arb_sub), (:/, :arb_div), (:hypot, :arb_
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat{P}}, Ptr{ArbFloat{P}}, Ptr{ArbFloat{P}}, Int), &z, &x, &y, P)
       return z
     end
-    function ($op){T<:ArbFloat}(x::T, y::T)
-      P = precision(T)
-      z = initializer(ArbFloat{P})
-      ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, &y, P)
-      return z
-    end
     ($op){P,Q}(x::ArbFloat{P}, y::ArbFloat{Q}) = ($op)(promote(x,y)...)
     ($op){T<:ArbFloat, F<:AbstractFloat}(x::T, y::F) = ($op)(x, convert(T, y))
     ($op){T<:ArbFloat, F<:AbstractFloat}(x::F, y::T) = ($op)(convert(T, x), y)
@@ -94,12 +64,6 @@ end
 function (*){P}(x::ArbFloat{P}, y::ArbFloat{P})
     z = initializer(ArbFloat{P})
     ccall(@libarb(arb_mul), Void, (Ptr{ArbFloat{P}}, Ptr{ArbFloat{P}}, Ptr{ArbFloat{P}}, Int), &z, &x, &y, P)
-    return z
-end
-function (*){T<:ArbFloat}(x::T, y::T)
-    P = precision(T)
-    z = initializer(ArbFloat{P})
-    ccall(@libarb(arb_mul), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &x, &y, P)
     return z
 end
 (*){T<:ArbFloat, F<:AbstractFloat}(x::T, y::F) = (*)(x, convert(T, y))
@@ -133,12 +97,6 @@ for (op,cfunc) in ((:+,:arb_add_si), (:-, :arb_sub_si), (:*, :arb_mul_si), (:/, 
       z = initializer(ArbFloat{P})
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat{P}}, Ptr{ArbFloat{P}}, Int, Int), &z, &x, y, P)
       return z
-    function ($op){T<:ArbFloat}(x::T, y::Int)
-      P = precision(T)
-      z = initializer(ArbFloat{P})
-      ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Int, Int), &z, &x, y, P)
-      return z
-    end
     end
   end
 end
@@ -163,12 +121,6 @@ for (op,cfunc) in ((:addmul,:arb_addmul), (:submul, :arb_submul))
     function ($op){P}(w::ArbFloat{P}, x::ArbFloat{P}, y::ArbFloat{P})
       z = initializer(ArbFloat{P})
       ccall(@libarb($cfunc), Void, (Ptr{ArbFloat{P}}, Ptr{ArbFloat{P}}, Ptr{ArbFloat{P}}, Ptr{ArbFloat{P}}, Int), &z, &w, &x, &y, P)
-      z
-    end
-    function ($op){T<:ArbFloat}(w::T, x::T, y::T)
-      P = precision(T)
-      z = initializer(ArbFloat{P})
-      ccall(@libarb($cfunc), Void, (Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Ptr{ArbFloat}, Int), &z, &w, &x, &y, P)
       z
     end
   end
