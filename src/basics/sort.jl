4404eb85f446e050b39ef43d8b66476b5a7fe27e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-01-24 15:49:52 -0500

    rename

--- src/basics/sort.jl
+++ src/basics/sort.jl
@@ -1,5 +1,5 @@
 sort{T<:ArbFloat}(xs::Vector{T}, lt::Function=<, rev::Bool=false) = sort(xs, alg=QuickSort, lt=lt, rev=rev)
-function sortIntervals{T<:ArbFloat}(xs::Vector{T}, rev::Bool=false)
+function sort_intervals{T<:ArbFloat}(xs::Vector{T}, rev::Bool=false)
    lessthan = rev ? pred : succ  
    return sort(xs, alg=MergeSort, lt=lessthan)
 end
