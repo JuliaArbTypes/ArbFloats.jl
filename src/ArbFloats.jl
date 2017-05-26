c61212e34cec29daf151b1f079d61a23d97a8ef9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-05-26 04:36:23 -0400

    Update ArbFloats.jl

--- src/ArbFloats.jl
+++ src/ArbFloats.jl
@@ -124,8 +124,10 @@ export ArbFloat,      # co-matched decimal rounding, n | round(hi,n,10) == round
        force_nonnegative_content, force_positive_content,
        sort_intervals                                         # uses weak total ordering over intervals 
 
-if !isdefined(:iszero)
+if isdefined(Base,:iszero)
   import Base:iszero
+else
+  export iszero
 end          
 
 NotImplemented(info::AbstractString="") = error(string("this is not implemented\n\t",info,"\n"))
