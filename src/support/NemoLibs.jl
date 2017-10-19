62891ed3a9362a7c2f5ccc4214758531cb068790
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-10-19 00:38:55 -0400

    fix 0.7 warnings

--- src/support/NemoLibs.jl
+++ src/support/NemoLibs.jl
@@ -1,21 +1,19 @@
-
+import Base.Sys: islinux, isbsd, isunix, isapple, iswindows
 # ensure the requisite libraries are available
 
 const noNemo = "Nemo.jl is not found:\n  Pkg.rm(\"Nemo\"); Pkg.add(\"Nemo\"); quit()\n  Pkg.rm(\"ArbFloats\");Pkg.add(\"ArbFloats\");"
 const reNemo = "Nemo.jl is not as expected:\n  Pkg.rm(\"Nemo\"); Pkg.add(\"Nemo\"); quit()\n  Pkg.rm(\"ArbFloats\");Pkg.add(\"ArbFloats\");"
 
-
 function package_directory(pkgName::String)
     pkgdir = Base.find_in_path(pkgName)
     nothing == pkgdir && throw(ErrorException(noNemo))
     return abspath(joinpath(split(pkgdir, pkgName)[1], pkgName))
-end    
+end
 
 function library_filepath(libsdir::String, filenames::Vector{String}, libname::String)
     libfile = filenames[ findfirst([startswith(x,libname) for x in filenames]) ]
     return joinpath( libsdir, libfile )
 end
-
 
 NemoLibsDir = abspath(joinpath( package_directory("Nemo"), "local/lib"))
 libFiles = readdir(NemoLibsDir)
@@ -28,17 +26,17 @@ isfile(libflint) || throw(ErrorException(reNemo))
 
 # prepare the libraries for use
 
-@static if (is_linux() || is_bsd())
+if (islinux() || isbsd() || isunix())
     libarb = String(split(libarb,".so")[1])
     libflint = String(split(libflint,".so")[1])
-end
-@static if is_apple()
+elseif isapple()
     libarb = String(split(libarb,".dynlib")[1])
     libflint = String(split(libflint,".dynlib")[1])
-end
-@static if is_windows()
+elseif iswindows()
     libarb = String(split(libarb,".dll")[1])
     libflint = String(split(libflint,".dll")[1])
+else
+    throw(ErrorException("Unrecognized Operating System"))
 end
 
 macro libarb(sym)
