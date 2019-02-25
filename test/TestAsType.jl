30d5a75f955380bdddd24ca16f1cf11d531c7a78
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-02-24 19:10:35 -0500

    Update TestAsType.jl

--- test/TestAsType.jl
+++ test/TestAsType.jl
@@ -7,8 +7,8 @@ af0 = zero(ArbFloat)
 af1 = one(ArbFloat)
 af2 = af1 + af1
 afhalf = af1 / 2
-afNaN = NAN
-afInf = INF
+afNaN = ArbFloats.NAN(ArbFloat{precision(ArbFloat)})
+afInf = ArbFloats.INF(ArbFloat)
 
 @test isnan(afNaN)
 @test isinf(afInf)
